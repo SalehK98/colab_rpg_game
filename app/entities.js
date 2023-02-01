23eb7c3dc3f5c9280b69675cb54d349de4bd497f
ArturLabenskyy <dodik.dod@hotmail.com>
2023-02-01 16:14:10 +0200

    Contract_class

--- app/entities.js
+++ app/entities.js
@@ -1,7 +1,6 @@
 // Monster and Player Entities
-
 class Entity {
-    constructor(name, health, strength, dexterity, defence, level, xp){
+    constructor(name, health, strength, dexterity, defence, level, xp) {
         this.name = name;
         this.health = health;
         this.strength = strength;
@@ -12,15 +11,17 @@ class Entity {
     }
 }
 
-class Player extends Entity{
-    constructor(name, health, strength, dexterity, defence, level, xp, potion){
+class Player extends Entity {
+    constructor(name, health, strength, dexterity, defence, level, xp, potion) {
         super(name, health, strength, dexterity, defence, level, xp);
         this.potion = potion;
     }
 }
 
-class Monster extends Entity{
-    constructor(name, health, strength, dexterity, defence, level, xp){
+class Monster extends Entity {
+    constructor(name, health, strength, dexterity, defence, level, xp) {
         super(name, health, strength, dexterity, defence, level, xp);
     }
 }
+
+module.exports = { Monster, Player };

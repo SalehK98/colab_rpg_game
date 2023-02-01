0c000a7f45533b5ad95dd0dfe67d5afbf0bf06c5
shellyalmo <shellyalmo@gmail.com>
2023-02-01 22:09:31 +0200

    fix bugs

--- app/database.js
+++ app/database.js
@@ -1,18 +1,40 @@
 //crud
-//class Database with crud methods, gets objects and stores them
+/**
+ class Database with crud methods, gets objects and stores them
+ * 
+ */
 class Database {
-  constructor() {}
+  /**
+   *
+   * @param {"monster"|"contracts"} key
+   * @param {any} instance
+   */
   create(key, instance) {
     localStorage.setItem(key, JSON.stringify(instance));
   }
-  get read() {
-    return { monster: JSON.parse(localStorage.getItem("monster")) };
+  fetch(key) {
+    return JSON.parse(localStorage.getItem(key));
   }
+  get read() {
+    return {
+      monster: this.fetch("monster"),
+      contracts: this.fetch("contracts"),
+    };
+  }
+  /**
+   *
+   * @param {"monster"|"contracts"} key
+   * @param {any} instance
+   */
   update(key, instance) {
     this.create(key, instance);
   }
+  /**
+   *
+   * @param {"monster"|"contracts"} key
+   */
   delete(key, instance) {
-    localStorage.removeItem(key);
+    localStorage.removeItem(key); //fix
   }
 }
 
@@ -20,7 +42,7 @@ let db = new Database();
 const testMonster = contracts[0].monster;
 console.log(testMonster);
 db.create("monster", testMonster);
-console.assert(db.read.monster.name == testMonster.name, "failed create");
+
 db.read.monster;
 console.assert(db.read.monster.name == testMonster.name, "failed read");
 
@@ -30,3 +52,17 @@ console.assert(db.read.monster.health == 42, "failed update");
 
 db.delete("monster", testMonster);
 console.assert(db.read.monster == undefined, "failed delete");
+
+db.create("contracts", contracts);
+db.read.contracts;
+console.assert(
+  JSON.stringify(db.read.contracts) == JSON.stringify(contracts),
+  "failed read"
+); //stringify for comparison
+db.update("contracts", contracts);
+console.assert(
+  JSON.stringify(db.read.contracts) == JSON.stringify(contracts),
+  "failed update"
+);
+db.delete("contracts", contracts);
+console.assert(db.read.contracts == undefined, "failed delete");

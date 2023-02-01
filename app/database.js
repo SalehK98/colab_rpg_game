7f2464f8c805bc888098cec1b1ca7c7e5010a2d4
shellyalmo <shellyalmo@gmail.com>
2023-02-01 21:13:44 +0200

    feat: db class for local storage, work in progress

---
+++ app/database.js
@@ -0,0 +1,22 @@
+//crud
+//class Database with crud methods, gets objects and stores them
+class Database {
+  constructor() {}
+  create() {}
+  read() {}
+  update() {}
+  delete() {}
+}
+
+let db = new Database();
+
+db.create(Monster);
+console.assert(db.kaflef == blajelfaj);
+db.read(Monster);
+console.assert(db.kaflef == blajelfaj);
+
+db.update(Monster);
+console.assert(db.kaflef == blajelfaj);
+
+db.delete(Monster);
+console.assert(db.kaflef == blajelfaj);
